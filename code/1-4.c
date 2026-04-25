55375034b53886b84a68cfafcb31bf42572aa5dd
rlaehdms13 <de060103@gmail.com>
2026-04-25 20:48:59 +0900

    Update 1-4.c

---
+++ code/1-4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int my_strcmp(const char *a, const char *b){
+  while(*a != '\0' && (*a == *b)){
+    a++;
+    b++;
+  }
+  return (int)(*a - *b);
+}
+int main(){
+  printf("%d\n", my_strcmp("abc", "abcd"));
+  printf("%d\n", my_strcmp("abc", "abc" ));
+  printf("%d\n", my_strcmp("b"  , "a"   ));
+
+  return 0;
+}
